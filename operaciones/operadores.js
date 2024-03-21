acd5959c301a008e5c78f880ffe6a49da00c101c
Diana_morales <dianacarolina.morales82@gmail.com>
2024-03-21 17:46:09 -0500

    archivos nuevos

---
+++ operaciones/operadores.js
@@ -0,0 +1,22 @@
+// operadores arimeticos basicos
+let counter =100;
+
+//1.suma
+
+couster = counter + 20; // 120 
+console.log(couster);
+couster +=20; //
+console.log(couster);
+  
+//resta 
+
+couster = counter - 50; // 120 
+console.log(couster);
+couster -=20; //
+console.log(couster);
+
+let number1= 70;
+
+let resta = number1 - 20;
+
+console.log(number1);
